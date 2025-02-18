dd75775589273c409b6d4749100242a54a2cf199
Tony <federicoaguirre134@gmail.com>
2025-02-17 20:11:22 -0600

    bug fix navbar for production bug

--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -4,7 +4,7 @@ import Button from './components/Button'
 
 function Hero() {
   return (
-    <div className="w-full min-h-[416] bg-silver grid grid-cols-1 md:grid-cols-2 place place-items-center gap-10 py-20 md:py-0">
+    <div className="w-full min-h-[416px] bg-silver grid grid-cols-1 md:grid-cols-2 place place-items-center gap-10 py-20 md:py-0">
       <div className="flex flex-col gap-6 items-center justify-center mx-8">
         <h1 className='text-5xl font-bold text-greyDark'>Lessons and insights<br/> <span className='text-darkLime'>from 8 years</span></h1>
         <p className='text-greyText'>
